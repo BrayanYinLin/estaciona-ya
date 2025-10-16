35486b277485e7febb0ae4a1466ea24e6c6691bd
QuevedoJorge <jorgequeve.job@gmail.com>
2025-10-16 13:50:02 -0500

    fix(client): alert on password changed

--- apps/client/src/modules/user/components/ChangePasswordForm.tsx
+++ apps/client/src/modules/user/components/ChangePasswordForm.tsx
@@ -1,4 +1,4 @@
-import type { FormEvent } from 'react'
+import { useEffect, useState, type FormEvent } from 'react'
 import { PasswordSection } from './PasswordSection'
 import { AlertSuccess } from '@shared/components/AlertSuccess'
 import { AlertError } from '@shared/components/AlertError'
@@ -6,6 +6,7 @@ import { useChangePassword } from '@user/hooks/useChangePasword'
 
 export function ChangePasswordForm() {
   const { error, success, loading, changePassword } = useChangePassword()
+  const [showAlert, setShowAlert] = useState(true)
 
   const handlePasswordChange = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -13,11 +14,16 @@ export function ChangePasswordForm() {
     const oldPassword = data.get('oldPassword') as string
     const newPassword = data.get('newPassword') as string
 
-    await changePassword({
-      oldPassword,
-      newPassword
-    })
+    await changePassword({ oldPassword, newPassword })
+    setShowAlert(true)
   }
+
+  useEffect(() => {
+    if ((success || error) && showAlert) {
+      const timer = setTimeout(() => setShowAlert(false), 3000)
+      return () => clearTimeout(timer)
+    }
+  }, [success, error, showAlert])
 
   return (
     <>
@@ -28,10 +34,13 @@ export function ChangePasswordForm() {
       >
         <div className="flex flex-col gap-5 mt-5 lg:mt-0 col-span-1">
           <PasswordSection />
-          {success && !loading && (
-            <AlertSuccess message="Se restableció la contraseña correctamente." />
-          )}
-          {error && <AlertError message={error} />}
+          {showAlert &&
+            !loading &&
+            (success ? (
+              <AlertSuccess message="Se restableció la contraseña correctamente." />
+            ) : error ? (
+              <AlertError message={error} />
+            ) : null)}
           <button type="submit" className="btn btn-outline btn-primary w-fit">
             Cambiar contraseña
           </button>
