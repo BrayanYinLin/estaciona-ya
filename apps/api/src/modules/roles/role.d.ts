d10776ba5d3f68568f598e999f0c75b3b77eb539
Brayan Yin Lin <brayanyl250206@gmail.com>
2025-10-19 21:56:08 -0500

    chore(api): role repository logic added

--- apps/api/src/modules/roles/role.d.ts
+++ apps/api/src/modules/roles/role.d.ts
@@ -1,5 +1,11 @@
-import { RoleDto } from './schemas/response_role.schema'
+import { RoleDto, RoleWithPermissionsDto } from './schemas/response_role.schema'
 
 export interface RoleRepository {
   findRoleByName(name: string): Promise<RoleDto | null>
+  findRoleByIdWithPermissions(
+    id: number
+  ): Promise<RoleWithPermissionsDto | null>
+  findRoleByNameWithPermissions(
+    name: string
+  ): Promise<RoleWithPermissionsDto | null>
 }
--- apps/api/src/modules/roles/role.repository.ts
+++ apps/api/src/modules/roles/role.repository.ts
@@ -1,10 +1,52 @@
 import { Repository } from 'typeorm'
 import { RoleRepository } from './role'
 import { Role } from './entities/role.entity'
-import { RoleDto } from './schemas/response_role.schema'
+import { RoleDto, RoleWithPermissionsDto } from './schemas/response_role.schema'
 
 export class RoleRepositoryImpl implements RoleRepository {
   constructor(private readonly repository: Repository<Role>) {}
+
+  async findRoleByNameWithPermissions(
+    name: string
+  ): Promise<RoleWithPermissionsDto | null> {
+    const role = await this.repository.findOne({
+      where: { name },
+      relations: {
+        permissions: true
+      }
+    })
+
+    if (!role) return null
+
+    const { id, name: nameRecovered, permissions } = role
+
+    return {
+      id,
+      name: nameRecovered,
+      permissions
+    }
+  }
+
+  async findRoleByIdWithPermissions(
+    id: number
+  ): Promise<RoleWithPermissionsDto | null> {
+    const role = await this.repository.findOne({
+      where: { id },
+      relations: {
+        permissions: true
+      }
+    })
+
+    if (!role) return null
+
+    const { id: idRecovered, name, permissions } = role
+
+    return {
+      id: idRecovered,
+      name,
+      permissions
+    }
+  }
 
   async findRoleByName(name: string): Promise<RoleDto | null> {
     const role = await this.repository.findOneBy({ name })
